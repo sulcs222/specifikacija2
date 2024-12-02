0fa837ed43dbb1bd82e6df87a1987f9492ad0338
sulcs222 <emils.markuss.sulcs@gmail.com>
2024-12-02 11:11:11 +0200

    Add files via upload

---
+++ datorika3000.py
@@ -0,0 +1,8 @@
+import random
+
+garums1 = int(input("Ievadiet minimālo paroles garumu!"))
+garums2 = int(input("Ievadiet maksimālo paroles garumu!"))
+
+for i in range(garums1, garums2):
+    skaitlis = random,randint(1, 10)
+    print (skaitlis, end="")
